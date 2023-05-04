d7082a37c9fd1ecb19b2985d69b0f07221a7e869
TokyFy <tokyfydev@gmail.com>
2023-05-04 11:57:49 +0200

    :zap: Finish project

---
+++ day_1/index.js
@@ -0,0 +1,36 @@
+// console.log("hello");
+// console.log(17);
+// console.log(1.7);
+// console.log(true);
+// console.log("hello" , "worlds")
+
+// var nom = "Definir";
+// nom = "Bonbon";
+// nom = "Jedica";
+
+// let color = "black";
+
+// const cube = "square";
+
+// comparaison
+
+// console.log("2" === 2) // false;
+
+// console.log("2" != 2 ) // false;
+
+// console.log("2" !== 2 ) // true;
+
+// console.log(!true) // false;
+
+// console.log(3 >= 4);
+
+
+// CONDITION
+
+var age = 17
+
+if( age <= 18 ) {
+    console.log(true);
+} else {
+    console.log(false);
+}
