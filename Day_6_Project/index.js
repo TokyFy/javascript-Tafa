3e36245fa79dd8632cb6c09bfefc4d15cf54740d
TokyFy <tokyfydev@gmail.com>
2023-05-04 11:56:50 +0200

    :zap: Finish project

---
+++ Day_6_Project/index.js
@@ -0,0 +1,28 @@
+const nameInput = document.getElementById("name");
+const mailInput = document.getElementById("mail");
+const cinInput = document.getElementById("cin");
+const phoneInput = document.getElementById("phone");
+const submit = document.getElementById("submit");
+
+[nameInput , mailInput , cinInput , phoneInput].forEach(
+    el => {
+        el.onkeyup = function (ev) {
+            document.getElementById(`p-${el.getAttribute("id")}`).innerText =
+                ev.target.value
+            el.style.borderColor = "black"
+        }
+    }
+)
+
+function validateForm() {
+    [nameInput , mailInput , cinInput , phoneInput].forEach(
+        el => {
+            if(el.value === "") {
+                el.style.borderColor = "red"
+            }
+        }
+    )
+}
+
+submit.onclick = validateForm;
+
