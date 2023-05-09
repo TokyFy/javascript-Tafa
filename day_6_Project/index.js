6629c1a1c503130a1d01b8b8140c4cac6b151356
TokyFy <tokyfydev@gmail.com>
2023-05-09 15:21:25 +0200

    :sparkles: finish project user

--- day_6_Project/index.js
+++ day_6_Project/index.js
@@ -4,24 +4,68 @@ const cinInput = document.getElementById("cin");
 const phoneInput = document.getElementById("phone");
 const submit = document.getElementById("submit");
 
-[nameInput , mailInput , cinInput , phoneInput].forEach(
-    el => {
-        el.onkeyup = function (ev) {
-            document.getElementById(`p-${el.getAttribute("id")}`).innerText =
-                ev.target.value
-            el.style.borderColor = "black"
-        }
-    }
-)
+const table = document.querySelector("#table-list tbody");
 
-function validateForm() {
-    [nameInput , mailInput , cinInput , phoneInput].forEach(
-        el => {
-            if(el.value === "") {
-                el.style.borderColor = "red"
-            }
-        }
-    )
+
+const themeToggler = document.getElementById("themeToggler");
+
+themeToggler.onclick = function (ev) {
+    ev.preventDefault();
+    let theme = document.documentElement.dataset.theme;
+
+    document.querySelector("#themeToggler .input").classList.toggle("checked");
+
+    console.log(theme);
+    if(theme === "Dark") {
+        document.documentElement.dataset.theme = "Light"
+    } else {
+        document.documentElement.dataset.theme = "Dark"
+    }
+};
+
+
+for (let el of [nameInput, mailInput, cinInput, phoneInput]) {
+    el.onkeyup = function (ev) {
+        ev.target.style.borderColor = "black";
+    }
 }
 
-submit.onclick = validateForm;
+function validateAndGetForm() {
+    let formData = {};
+
+
+    for (let el of [nameInput, mailInput, cinInput, phoneInput]) {
+        if (el.value === "") {
+            el.style.borderColor = "red";
+            formData = null;
+            break;
+        }
+        formData[el.getAttribute("name")] = el.value;
+    }
+
+    return formData;
+}
+
+function saveUserInfo(name, mail, cin, phone) {
+    table.innerHTML += `
+    <tr>
+    <td>${name}</td>
+    <td>${mail}</td>
+    <td>${phone}</td>
+    <td>${cin}</td>
+    </tr>`
+}
+
+function clearForm() {
+    for (let el of [nameInput, mailInput, cinInput, phoneInput]) {
+        el.value = "";
+    }
+}
+
+submit.onclick = function () {
+    const user = validateAndGetForm();
+    if(user) {
+        saveUserInfo(user.name , user.mail , user.cin , user.phone)
+        clearForm()
+    }
+};
